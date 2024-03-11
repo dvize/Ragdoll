274cc42fa1e74b34bc81aec630f08e174bf605f2
dvize <dvize404@gmail.com>
2024-03-11 12:26:44 -0700

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using Aki.Reflection.Patching;
+using BepInEx;
+using BepInEx.Configuration;
+using EFT;
+using HarmonyLib;
+
+namespace RAG
+{
+    [BepInPlugin("com.dvize.Ragdoll", "dvize.Ragdoll", "1.0.0")]
+    //[BepInDependency("com.spt-aki.core", "3.7.4")]
+    public class RagPlugin : BaseUnityPlugin
+    {
+        private static Harmony _harmony;
+        internal void Awake()
+        {
+            _harmony = new Harmony("com.dvize.Ragdoll");
+            _harmony.PatchAll(typeof(ApplyHitPatch));
+
+            new NewGamePatch().Enable();
+        }
+
+        
+    }
+
+    //re-initializes each new game
+    internal class NewGamePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod() => typeof(GameWorld).GetMethod(nameof(GameWorld.OnGameStarted));
+
+        [PatchPrefix]
+        public static void PatchPrefix()
+        {
+            RagComponent.Enable();
+        }
+    }
+}
+
---
+++ RagComponent.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using Comfort.Common;
+using EFT;
+using EFT.UI.Ragfair;
+using RootMotion.FinalIK;
+using UnityEngine;
+
+namespace RAG
+{
+    internal class RagComponent : MonoBehaviour
+    {
+        internal static ManualLogSource Logger;
+        internal static GameWorld gameWorld;
+        internal static Player player;
+
+        private void Awake()
+        {
+            if (Logger == null)
+                Logger = BepInEx.Logging.Logger.CreateLogSource(nameof(RagComponent));
+        }
+
+        private void Start()
+        {
+            player = gameWorld.MainPlayer;
+
+            EFTHardSettings.Instance.DEBUG_CORPSE_PHYSICS = true;
+
+            Player.OnPlayerDeadStatic += Player_OnPlayerDeadStatic;
+        }
+
+        private void Player_OnPlayerDeadStatic(Player player, IPlayer arg2, DamageInfo damageInfo, EBodyPart bodypart)
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        public static void Enable()
+        {
+            if (Singleton<IBotGame>.Instantiated)
+            {
+                //add component to gameWorld
+                gameWorld = Singleton<GameWorld>.Instance;
+                gameWorld.gameObject.AddComponent<RagComponent>();
+                Logger.LogDebug("RagComponent Enabled");
+
+            }
+        }
+
+        bool IsKeyPressed(KeyboardShortcut key)
+        {
+            if (!UnityInput.Current.GetKeyDown(key.MainKey))
+            {
+                return false;
+            }
+            foreach (var modifier in key.Modifiers)
+            {
+                if (!UnityInput.Current.GetKey(modifier))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
